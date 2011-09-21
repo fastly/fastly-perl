16420273ce99900086648b4a2d7fe1d4535eebcd
Simon Wistow <simon@thegestalt.org>
2011-09-20 18:24:03 -0700

    Add list_services

--- lib/Fastly/Service.pm
+++ lib/Fastly/Service.pm
@@ -8,20 +8,20 @@ Fastly::Service->mk_accessors(qw(id customer name created_at updated_at deleted_
 sub stats {
     my $self = shift;
     my $type = shift || "all";
-    die "You must be fully authed to get stats" unless $self->fetcher->fully_authed;
+    die "You must be authed to get stats" unless $self->fetcher->authed;
     die "Unknown stats type $type" unless grep { $_ eq $type } qw(minutely hourly daily all);
     return $self->fetcher->client->get($self->get_path($self->id)."/stats/".$type);    
 }
 
 sub purge_all {
     my $self = shift;
-    die "You must be fully authed to purge everything in a service" unless $self->fetcher->fully_authed;
+    die "You must be authed to purge everything in a service" unless $self->fetcher->authed;
     return $self->fetcher->client->put($self->get_path($self->id)."/purge_all");
 }
 
 sub versions {
     my $self  = shift;
-    die "You must be fully authed to get the versions for a service" unless $self->fetcher->fully_authed;
+    die "You must be authed to get the versions for a service" unless $self->fetcher->authed;
     my $fetcher  = $self->fetcher;
     my $versions = $self->{versions};
     my @versions;
@@ -33,18 +33,25 @@ sub versions {
 
 sub version {
     my $self = shift;
-    die "You must be fully authed to get the current version" unless $self->fetcher->fully_authed;
+    die "You must be authed to get the current version" unless $self->fetcher->authed;
     my @list = $self->versions;
     return $list[-1];
 }
 
 package Fastly;
 
+sub list_services {
+    my $self  = shift;
+    my %opts  = @_;
+    my $class = "Fastly::Service";
+    return $self->list($class, %opts);
+}
+
 sub search_services {
     my $self  = shift;
     my %opts  = @_;
     my $class = "Fastly::Service"; 
-    die "You must be fully authed to search for a $class" unless $self->fully_authed;
+    die "You must be authed to search for a $class" unless $self->authed;
     my $hash    = $self->client->get($class->post_path."/search", %opts);
     return undef unless $hash;
     return $class->new($self, %$hash);
