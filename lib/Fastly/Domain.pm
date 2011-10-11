37c713f8bb066afc48e344155399181932dc7fad
Simon Wistow <simon@thegestalt.org>
2011-10-11 15:35:28 -0700

    Add deleted_at

--- lib/Fastly/Domain.pm
+++ lib/Fastly/Domain.pm
@@ -3,7 +3,7 @@ package Fastly::Domain;
 use strict;
 use base qw(Fastly::BelongsToServiceAndVersion);
 
-Fastly::Domain->mk_accessors(qw(service version name created_at updated_at id owner_id));
+Fastly::Domain->mk_accessors(qw(service version name created_at updated_at deleted_at));
 
 =head1 NAME
 
