2daf4904381e902241e956f4e0eb6e7365267b64
Simon Wistow <simon@thegestalt.org>
2012-01-27 09:09:04 -0800

    Since you can now get the current_customer with the API key we need to get the customer name

--- t/Helper.pm
+++ t/Helper.pm
@@ -14,7 +14,7 @@ sub login_opts {
         my $key = "FASTLY_TEST_BASE_".uc($what);
         $opts{"base_${what}"} = $ENV{$key} if defined $ENV{$key};
     }
-    my @required = ("full" eq $mode) ? qw(user name password customer) : qw(api_key);
+    my @required = ("full" eq $mode) ? qw(user name password customer) : qw(api_key customer);
     foreach my $what (@required) {
         my $key = "FASTLY_TEST_".uc($what);
         die "You haven't set the environment variable $key\n" unless defined $ENV{$key};
