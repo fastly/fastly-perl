816b8adedc54bda103614ead879bf718eae75ae2
Katsuyuki Omuro <komuro@fastly.com>
2022-06-27 15:29:29 +0900

    [Perl] Add previously removed build CI files

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$TRAVIS_TAG" == "" ]; then
+    exit
+fi
+
+if [ "$PERLBREW_PERL" != "5.28" ]; then
+    exit
+fi
+
+cpanm -nq Dist::Milla
+
+mkdir -p ~/.dzil
+cat <<EOF > ~/.dzil/config.ini
+[%PAUSE]
+username = FASTLY
+password = $PAUSE_PASS
+EOF
+
+VERSION=$(echo $TRAVIS_TAG | sed -e 's/^v//' | sed -e 's/-TRIAL.*$//' )
+if [[ $TRAVIS_TAG =~ "-TRIAL" ]]; then
+  TRIAL=--trial
+fi
+
+echo "\ny" | V=$VERSION milla release $TRIAL
